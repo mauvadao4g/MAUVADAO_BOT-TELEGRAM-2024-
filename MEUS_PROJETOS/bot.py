9050b2f7acf106c7998128cf6703d11c8f75ed4e
Mauvadao <108885597+mauvadao4g@users.noreply.github.com>
2024-01-16 15:35:02 -0300

    Add files via upload

---
+++ MEUS_PROJETOS/bot.py
@@ -0,0 +1,37 @@
+import telebot, csv
+# from decouple import config
+from telebot import types
+import requests
+
+bot = telebot.TeleBot(token='SEU_TOKEN_TELEGRAM', parse_mode='HTML')
+
+
+def salvar(id_telegram):
+    with open('ids_telegram.csv', 'a') as ids:
+        e = csv.writer(ids)
+        e.writerow([id_telegram])
+
+@bot.message_handler(commands=['start', 'inicio', 'menu'])
+def start(message):
+    salvar(message.from_user.id)
+    bot.send_message(message.chat.id, 'Olá, tudo bom ?\nque bom que voce veio conhecer meus aplicativos\nNabegue e procure um que lhe agrade\nObrigado...\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  fazendo download de um arquivo 
+@bot.message_handler(commands=['download'])
+def download(message):
+    doc = open('teste.pdf', 'rb')
+    bot.send_document(message.chat.id, doc)
+
+bot.polling()
---
+++ MEUS_PROJETOS/enviar.py
@@ -0,0 +1,41 @@
+import telebot
+
+bot = telebot.TeleBot(token='SEU_TOKEN_BOT', parse_mode='HTML')
+
+
+bot.send_message(ENVIAR_MSG, ' Opa, tudo certo ?\n \
+Olhe os preços que tenho pra você')
+
+bot.send_message(ENVIAR_MSG, text='🔰CLOUDFRONT OFF❌️🔰 \
+\nVIVO 🔵 \
+\nTIM  🔵 \
+\nCLARO 🔴❌️❌️❌️ \
+\nFLUK🟡 ⁉️⁉️ \
+\nVALOR DO PAINEL  ACESSO POR MÊS . \
+\n• 🛒5 LONGS  25,00$🇧🇷 \
+\n• 🛒10 LONGS  35,00$🇧🇷 \
+\n• 🛒20 LONGS  45,00$🇧🇷 \
+\n• 🛒30 LONGS  50,00$🇧🇷 \
+\n• 🛒50 LONGS   60,00$ 🇧🇷 \
+\n• 🛒60 LONGS   70,00$🇧🇷 \
+\n• 🛒70 LONGS   80,00$🇧🇷 \
+\n• 🛒80 LONGS   90,00$🇧🇷 \
+\n• 🛒100LONGS 100,00$🇧🇷 \
+\n• 🛒130LONGS 130,00$🇧🇷 \
+\n• 🛒160LONGS 160,00$🇧🇷 \
+\n• 🛒200LONGS 220,00$🇧🇷 \
+\n• 🛒250LONGS 230,00$🇧🇷 \
+\n• 🛒300LONGS 250,00$🇧🇷 \
+\n• (+100 Login Ganha App com seu tema e sua logo) \
+                     \n\nEi!!!\nTu gostou \nMe chama pra contratar: @mauvadao')
+
+
+
+
+
+
+#  fazendo download de um arquivo 
+
+bot.send_message(ENVIAR_MSG, 'Baixe o aplicativo e crie um tese')
+doc = open('teste.pdf', 'rb')
+bot.send_document(ENVIAR_MSG, doc)
