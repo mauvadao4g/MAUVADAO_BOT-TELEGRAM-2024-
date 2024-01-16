a2306a9050ea227628d1159bbbafe38553e72000
Mauvadao <108885597+mauvadao4g@users.noreply.github.com>
2024-01-16 18:32:56 -0300

    Add files via upload

---
+++ ARQUIVOS_AULAS_BOT/AULAS_01/bot-01.py
@@ -0,0 +1,24 @@
+import telebot, csv
+# from decouple import config
+from telebot import types
+import requests
+
+bot = telebot.TeleBot(token='6174988485:AAEjYdZArdNtM7VUjsFw3hbjegmrrDRxsFk', parse_mode='HTML')
+
+
+def salvar(id_telegram):
+    with open('ids_telegram.csv', 'a') as ids:
+        e = csv.writer(ids)
+        e.writerow([id_telegram])
+
+@bot.message_handler(commands=['start', 'inicio', 'menu'])
+def start(message):
+    salvar(message.from_user.id)
+    bot.send_message(message.chat.id, 'Olá, tudo bom ?')
+
+@bot.message_handler(commands=['download'])
+def download(message):
+    doc = open('teste.pdf', 'rb')
+    bot.send_document(message.chat.id, doc)
+
+bot.polling()
